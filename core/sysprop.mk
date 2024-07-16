3b7c830aeec735bf06ea18f6f07f5594f9962bc5
rahul <rahul@aospa.co>
2024-07-16 21:54:01 +0000

    build/core: Set BUILD_DISPLAY_ID as defined for user builds.
    * This information is kind of pointless, especially when we rely on PenguinOS version numbers.
    * Instead of differentiating, just set it to one value.
    * Visually more cleaner.

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -218,20 +218,7 @@ BUILD_THUMBPRINT :=
 BUILD_DESC := $(TARGET_PRODUCT)-$(TARGET_BUILD_VARIANT) $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER_FROM_FILE) $(BUILD_VERSION_TAGS)
 
 # BUILD_DISPLAY_ID is shown under Settings -> About Phone
-ifeq ($(TARGET_BUILD_VARIANT),user)
-  # User builds should show:
-  # release build number or branch.buld_number non-release builds
-
-  # Dev. branches should have DISPLAY_BUILD_NUMBER set
-  ifeq (true,$(DISPLAY_BUILD_NUMBER))
-    BUILD_DISPLAY_ID := $(BUILD_ID).$(BUILD_NUMBER_FROM_FILE) $(BUILD_KEYS)
-  else
-    BUILD_DISPLAY_ID := $(BUILD_ID) $(BUILD_KEYS)
-  endif
-else
-  # Non-user builds should show detailed build information
-  BUILD_DISPLAY_ID := $(BUILD_DESC)
-endif
+BUILD_DISPLAY_ID := $(BUILD_ID) $(BUILD_KEYS)
 
 # TARGET_BUILD_FLAVOR and ro.build.flavor are used only by the test
 # harness to distinguish builds. Only add _asan for a sanitized build
